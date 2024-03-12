f62f0f77a2d30d4118d234e9bb3a8a7ba23fe6b7
Vishal Jain <61986769+thevishalj@users.noreply.github.com>
2024-03-12 16:36:39 +0530

    Firebase Setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCclEur8b3bytVuM0Cb7k9Gf95QjN89qgg",
+        authDomain: "gpu-marketplace.firebaseapp.com",
+        projectId: "gpu-marketplace",
+        storageBucket: "gpu-marketplace.appspot.com",
+        messagingSenderId: "1009616232297",
+        appId: "1:1009616232297:web:de59b116ff0784a06b27f8",
+        measurementId: "G-GGN66TJW17"
+      }
+};
