776b032174efbb856e2e69059d6a4f961feaff39
Vishal Jain <61986769+thevishalj@users.noreply.github.com>
2024-03-12 17:49:25 +0530

    Added required components and added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from 'angularfire2'; 
 import { AngularFireDatabaseModule } from 'angularfire2/database'; 
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import { HomepageComponent } from './home-page/homepage.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout-page/checkout.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    HomepageComponent,
+    ProductsPageComponent,
+    ShoppingCartComponent,
+    CheckoutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +39,16 @@ import { AppComponent } from './app.component';
     // AngularFireModule.initializeApp(environment.firebase),
     // AngularFireDatabaseModule,
     // AngularFireAuthModule
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent },
+      { path: 'products', component: ProductsPageComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckoutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'login', component: LoginPageComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
