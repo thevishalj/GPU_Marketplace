7f5aad4b8d01df89711b7b509af383aeb01c4612
Vishal Jain <61986769+thevishalj@users.noreply.github.com>
2024-03-12 16:07:27 +0530

    Angular Version Change
    Downgrade version from 17 to 16 due to lack of support and resources for v17 changes. Additionally, the introduction of the new standalone component feature in v17 proved to be tricky to understand.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'GPU_Marketplace';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from './../environments/environment';
-import { AngularFireModule } from 'angularfire2'; 
-import { AngularFireDatabaseModule } from 'angularfire2/database'; 
-import { AngularFireAuthModule } from 'angularfire2/auth'; 
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,9 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
